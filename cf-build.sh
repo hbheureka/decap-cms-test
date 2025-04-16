1746f40af516a6a584b93f48b64a4d0865030765
Han Bonsink <53989657+hbheureka@users.noreply.github.com>
2025-04-16 15:17:31 +0200

    Test if BASE_URL is set

--- cf-build.sh
+++ cf-build.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+if [ -z ${BASE_URL} ]; then
+  BASE_URL=${CF_PAGES_URL}
+fi
+
 if [ "$CF_PAGES_BRANCH" == "main" ]; then
   git fetch --unshallow && hugo -b $BASE_URL --gc --minify --templateMetrics --templateMetricsHints --forceSyncStatic --enableGitInfo
 else
